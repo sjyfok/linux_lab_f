2bc04c120f5dbdc3460a57b42fc99063d50e1e39
lab <lab@lab-laptop.(none)>
2016-12-15 23:23:34 +0800

    sem.c have some problem

--- ch4/sem.c
+++ ch4/sem.c
@@ -6,6 +6,7 @@
 #include <stdio.h>
 #include <errno.h>
 #include <time.h>
+#include <stdlib.h>
 
 #define	MAXSEM	5
 
@@ -26,13 +27,13 @@ int main(void)
 	pid_t pid;
 	
 	array = mmap(NULL, sizeof(int)*MAXSEM, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
-	sum = mmap(NULL, sizeof(int), PROT_READ | PORT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
-	set = mmap(NULL, sizeof(int), PROT_READ | PORT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
-	get = mmap(NULL, sizeof(int), PROT_READ | PORT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+	sum = mmap(NULL, sizeof(int), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+	set = mmap(NULL, sizeof(int), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+	get = mmap(NULL, sizeof(int), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
 
-	fullid = semget(100, 1, IPC_CREATE);
-	emptyid = semget(101, 1, IPC_CREATE);
-	mutxid = semget(102, 1, IPC_CREATE);
+	fullid = semget(100, 1, IPC_CREAT);
+	emptyid = semget(101, 1, IPC_CREAT);
+	mutxid = semget(102, 1, IPC_CREAT);
 	arg.val = 0;
 	if (semctl(fullid, 0, SETVAL, arg) == -1)
 	{
@@ -65,15 +66,16 @@ int main(void)
 		int i = 0;
 		while (i < 100)
 		{
-			semop(mutxid, &P, 1);
 			semop(emptyid, &P, 1);
-			array[*(set)%MAXSEM] = i+1;
-			*set ++;
-			semop(fullid, &V, 1);
+			semop(mutxid, &P, 1);
+			array[(*set)%MAXSEM] = i+1;
+			printf("P gen a num %d\n", array[(*set)%MAXSEM]);
+			(*set) ++;
 			semop(mutxid, &V, 1);
+			semop(fullid, &V, 1);
 			i ++;
+			sleep(1);
 		}
-		sleep(5);
 		printf("product is exit\n");
 		exit(0);
 	}
@@ -81,10 +83,70 @@ int main(void)
 	{
 		if (pid > 0)
 		{
+			pid = fork();
+			if (pid == 0)
+			{
+				while(1)
+				{
+					semop(fullid, &P, 1);
+					semop(mutxid, &P, 1);
+					if (*get  == 100)
+					{
+						break;
+					}
+					*sum += array[(*get)%MAXSEM];
+					printf("A get number %d\n", array[(*get)%MAXSEM]);
+					(*get) ++;
+					if (*get == 100)
+					{
+						printf("The sum = %d\n", sum);
+					}
+					semop(mutxid, &V, 1);
+					semop(emptyid, &V, 1);
+					sleep(1);
+				}
+				semop(mutxid, &V, 1);
+				semop(emptyid, &V, 1);
+				printf("A is over\n");
+				exit(0);
+			}
+			else
+			{
+				if (pid > 0)
+				{
+					pid = fork();
+					if (pid == 0)
+					{
+						while(1)
+						{
+							semop(fullid, &P, 1);
+							semop(mutxid, &P, 1);
+							if (*get == 100)
+								break;
+							*sum += array[(*get)%MAXSEM];
+							printf("B get number %d\n", array[(*get)%MAXSEM]);
+							(*get) ++;
+							if (*get == 100)
+								printf("sum = %d\n", *sum);
+							semop(emptyid, &V, 1);
+							semop(mutxid, &V, 1);
+							sleep(1);
+						}
+						semop(mutxid, &V, 1);
+						semop(emptyid, &V, 1);
+						printf("B is over\n");
+						exit(0);
+					}
+				}
+				else
+				{
+					perror("fail to fork\n");
+				}
+			}
 		}
 		else
 		{
-			perror("Fail to fork");
+			perror("Fail to fork\n");
 		}
 	}
 }
