d321d75ce7605cbc75777cefe881e1c38172b194
lab <lab@lab-laptop.(none)>
2016-12-20 21:43:43 +0800

    shm is done

---
+++ ch4/sharememread.c
@@ -0,0 +1,36 @@
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <error.h>
+#include <string.h>
+
+typedef struct {
+	char name[4];
+	int age;
+}people;
+
+int main(int argc, char **argv)
+{
+	int shmid; 
+	people *ptr;
+
+	shmid = shmget(100, sizeof(people), IPC_CREAT | 0600);
+	if (shmid < 0)
+	{
+		perror("fail shmget");
+		exit(0);
+	}
+	ptr = (people*)shmat(shmid, NULL, 0);
+	printf("name:%s\n", ptr->name);
+	printf("age:%d\n", ptr->age);
+	shmid = shmdt(ptr);
+	if (shmid < 0)
+	{
+		perror("fail to shmdt");
+		exit(0);
+	}
+	return 0;
+}
